f76a3a675b2194449f6224ffb1d723e0724960d1
germauricio <mpauluk@est.frba.utn.edu.ar>
2020-06-03 09:51:00 -0300

    circleci

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,32 +1,27 @@
 import React from 'react';
 import '../App.css';
 
-class PokemonList extends  React.Component {
-
-    render() {
-        return (
-            this.props.pokemons
-                .map( ( pokemon ) => {
-                    let imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`
-                    return(
-                        <div className = "box" key = {pokemon.id}>
-				        	<div className = "col-md-3 col-sm-6">
-						        <div className = "card">
-                                    <div className="card-tittle" data-testid = "pokeName">Nombre: {pokemon.name}</div>
-                                    <img 
-                                        src = {imageUrl}
-                                        className = "pokeImg"
-                                        width = "175px"
-                                        alt = {"pokemon " + pokemon.id}
-                                    />
-                                    <div className="card-text" data-testid = "pokeID">Id: {pokemon.id}</div>
-                                </div>
+export default function PokemonList(props){
+    return (
+        props.pokemons.map(
+            ( pokemon ) => {
+                let imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`
+                return(
+                    <div className = "box" key = {pokemon.id}>
+                        <div className = "col-md-3 col-sm-6">
+                            <div className = "card" >
+                                <div className="card-tittle" data-testid = "pokeName">Nombre: {pokemon.name}</div>
+                                <img
+                                    src = {imageUrl}
+                                    className = "pokeImg"
+                                    width = "175px"
+                                    alt = {"pokemon " + pokemon.id}
+                                />
+                                <div className="card-text" data-testid = "pokeID">Id: {pokemon.id}</div>
                             </div>
                         </div>
-                    );
-                })
-        );
-    };
+                    </div>
+                );
+            })
+    );
 }
-
-export default PokemonList;
