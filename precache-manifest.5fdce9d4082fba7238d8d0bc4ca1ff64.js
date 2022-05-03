25cbb55fb57927b30895ea130c89effcfc6ebbb4
germauricio <mauricio.pauluk@mercadolibre.com>
2022-05-03 12:29:12 -0300

    Updates

---
+++ precache-manifest.5fdce9d4082fba7238d8d0bc4ca1ff64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8ddac6fedeb11c9f673f323d36176e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d5a82cf459ccbc0230b",
+    "url": "/static/css/main.2a346540.chunk.css"
+  },
+  {
+    "revision": "a323750a50a59b3a4a8c",
+    "url": "/static/js/2.fe106841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe106841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5a82cf459ccbc0230b",
+    "url": "/static/js/main.8fb143ec.chunk.js"
+  },
+  {
+    "revision": "7250a77dc59ce6b6d93d",
+    "url": "/static/js/runtime-main.59da08b4.js"
+  }
+]);
